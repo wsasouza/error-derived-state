8a35d76269c4b90a6a176e88781bacb41738c14a
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-08-02 07:55:21 -0300

    Derivative states in action

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,57 @@ interface Repo {
 
 
 export function App() {
-  const [repos, setRepos] = useState<Repo[]>([])
+  const [repos, setRepos] = useState<Repo[]>([]);
+  const [filteredRepos, setFilteredRepos] = useState<Repo[]>([]);
+  const [search, setSearch] = useState('');  
+
+  console.log('Render');
 
   useEffect(() => {
     fetch('https://api.github.com/users/wsasouza/repos')
       .then(response => response.json())
       .then(data => setRepos(data))
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    if (search.length > 0)
+      setFilteredRepos(repos.filter(repo => repo.name.includes(search)));
+  }, [search]);
 
   return (
     <div>
-      <input type="text" name='search' placeholder='buscar' />
+      <input 
+        type="text" 
+        name="search"
+        placeholder="buscar"
+        onChange={e => setSearch(e.target.value)}
+        value={search}
+      />
 
-      <ul>
-        {repos.map(repo => {
-          return (
-            <li key={repo.name}>
-              {repo.name}
-            </li>
-          )
-        })}
-      </ul>        
+      { search.length > 0 ? (
+          <ul>
+            {filteredRepos.map(repo => {
+              return (
+                <li key={repo.name}>
+                  {repo.name}
+                </li>
+              )
+            })}
+          </ul> 
+        ) : (
+          <ul>
+            {repos.map(repo => {
+              return (
+                <li key={repo.name}>
+                  {repo.name}
+                </li>
+              )
+            })}
+          </ul> 
+        )
+      }       
     </div>
-  )
+  );
 }
 
 
